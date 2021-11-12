3c1249a4afcd7914a6975f2f9eafc313519050e3
Lucas Nardon <lucaslnardon@gmail.com>
2021-11-11 23:13:48 -0300

    ✨ Add voice speed change and play/pause controls

--- main.js
+++ main.js
@@ -1,15 +1,34 @@
 const speaker = new SpeechSynthesisUtterance();
 
+// Check if browser has support
+if (!speaker) {
+  alert("Your browser does not support the SpeechSynthesisUtterance API");
+}
+
+//Stops speechSynthesis when user leaves page
+window.addEventListener("beforeunload", (e) => {
+  e.preventDefault();
+  speechSynthesis.cancel();
+});
+
 function playText() {
   speaker.text = document.getElementById("text").value;
   speechSynthesis.speak(speaker);
 }
 
 function stopText() {
-  speaker.stop();
+  speechSynthesis.cancel();
+  console.log(window.speechSynthesis.getVoices());
 }
 
 function handleSpeedChange() {
+  let voices = window.speechSynthesis.getVoices();
+  if (voices.length > 0) {
+    speaker.voice = voices[2];
+    speechSynthesis.speak(speaker);
+  }
+  speechSynthesis.cancel();
   let speed = document.getElementById("speed").value;
   speaker.rate = parseFloat(speed);
+  speechSynthesis.speak(speaker);
 }
