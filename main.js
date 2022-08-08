51f2691025b31800088d2b8ca144c322f30c407e
Lucas Nardon <lucaslnardon@gmail.com>
2022-08-08 00:01:08 -0300

    ✨ Add full API functionality

--- main.js
+++ main.js
@@ -2,32 +2,27 @@ const speaker = new SpeechSynthesisUtterance();
 
 // Check if browser has support
 if (!speaker) {
-  alert("Your browser does not support the SpeechSynthesisUtterance API");
+  alert("Your browser does not support the SpeechSynthesisUtterance API.");
 }
 
-//Stops speechSynthesis when user leaves page
+// Stops speechSynthesis when user leaves page
 window.addEventListener("beforeunload", (e) => {
   e.preventDefault();
   speechSynthesis.cancel();
 });
 
 function playText() {
-  speaker.text = document.getElementById("text").value;
-  speechSynthesis.speak(speaker);
+  window.speechSynthesis.cancel();
+  let text = document.getElementById("text").value;
+  var msg = new SpeechSynthesisUtterance(text);
+  window.speechSynthesis.speak(msg);
 }
 
 function stopText() {
   speechSynthesis.cancel();
-  console.log(window.speechSynthesis.getVoices());
 }
 
 function handleSpeedChange() {
-  let voices = window.speechSynthesis.getVoices();
-  if (voices.length > 0) {
-    speaker.voice = voices[2];
-    speechSynthesis.speak(speaker);
-  }
-  speechSynthesis.cancel();
   let speed = document.getElementById("speed").value;
   speaker.rate = parseFloat(speed);
   speechSynthesis.speak(speaker);
